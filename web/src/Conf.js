e2c4c738b256a2121f706d2b79e3e17f48cf4082
Love98 <git@love98.net>
2024-05-26 19:53:18 +0800

    fix: rollback local config

--- web/src/Conf.js
+++ web/src/Conf.js
@@ -13,12 +13,11 @@
 // limitations under the License.
 
 export const AuthConfig = {
-  // serverUrl: "https://door.casdoor.com",
-  serverUrl: "http://localhost:7001",
-  clientId: "8e49fe802f2aa73026c4",
-  appName: "app-build-in",
-  organizationName: "build-in",
-  redirectPath: "/callback",
+  serverUrl: "https://door.casdoor.com",
+  // serverUrl: "http://localhost:7001",
+  clientId: "af6b5aa958822fb9dc33",
+  appName: "app-casibase",
+  organizationName: "casbin",
 };
 
 export const IsDemoMode = false;
