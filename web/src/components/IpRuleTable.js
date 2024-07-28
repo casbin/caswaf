7f44e8a7909e94eea562f057e06dcfd815b37e77
Love98 <liaocai98@gmail.com>
2024-07-28 22:58:40 +0800

    update: remove 127.0.0.0/8 from lan cidr

--- web/src/components/IpRuleTable.js
+++ web/src/components/IpRuleTable.js
@@ -34,7 +34,7 @@ class IpRuleTable extends React.Component {
         {
           name: "lan cidr",
           operator: "is in",
-          value: "10.0.0.0/8,127.0.0.0/8,192.0.0.0/24",
+          value: "10.0.0.0/8,192.168.0.0/16",
         },
       ],
     };
